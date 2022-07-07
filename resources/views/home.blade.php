0dc828d925fbb96482fe1e8fe01371b9422a27ef
salamone-gius <salamone.gius@gmail.com>
2022-07-07 10:47:46 +0200

    header isolato e @incluso (non duplicato)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,29 +72,8 @@
         </style>
     </head>
     <body>
-        <header>
-            <div class="header-top">
-                <div class="flex-center">
-                    <h1 class="title">laravel - primi passi</h1>
-                </div>
-            </div>
-            <nav class="header-bottom">
-                <ul class="flex-center">
-                    <li>
-                        <a href="/">home</a>
-                    </li>
-                    <li>
-                        <a href="pag-1">pagina 1</a>
-                    </li>
-                    <li>
-                        <a href="pag-2">pagina 2</a>
-                    </li>
-                    <li>
-                        <a href="pag-3">pagina 3</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        {{-- include parte sempre dalla cartella views --}}
+        @include('partials.header')
         <main>
             <div class="main-content flex-center">
                 <h2 class="title">questa è la pagina <br> "home"</h2>
--- resources/views/pag-1.blade.php
+++ resources/views/pag-1.blade.php
@@ -72,29 +72,7 @@
         </style>
     </head>
     <body>
-        <header>
-            <div class="header-top">
-                <div class="flex-center">
-                    <h1 class="title">laravel - primi passi</h1>
-                </div>
-            </div>
-            <nav class="header-bottom">
-                <ul class="flex-center">
-                    <li>
-                        <a href="/">home</a>
-                    </li>
-                    <li>
-                        <a href="pag-1">pagina 1</a>
-                    </li>
-                    <li>
-                        <a href="pag-2">pagina 2</a>
-                    </li>
-                    <li>
-                        <a href="pag-3">pagina 3</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        @include('partials.header')
         <main>
             <div class="main-content flex-center">
                 <h2 class="title">questa è la pagina <br> "1"</h2>
